5b7f5489c8c453b14636cdb1db353972abe0ac72
Sherrina <robinsonsherrina@gmail.com>
2023-05-23 13:55:55 -0400

    updated property state changes in router

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -7,11 +7,28 @@ import Image from 'next/image';
 import { filterData, getFilterValues } from '../utils/filterData';
 
 const SearchFilters = () => {
+    /* Get filter data to use in property search */
     const [filters, setFilters] = useState(filterData);
 
-    const searchProperties = (filterValues) => {
+    const router = useRouter();
 
-    }
+    /* Update property state */
+    const searchProperties = (filterValues) => {
+        /*update url when state changes */
+        const path = router.pathname;
+        const { query } = router;
+
+        const values = getFilterValues(filterValues)
+
+        values.forEach((item) => {
+        if(item.value && filterValues?.[item.name]) {
+            query[item.name] = item.value
+        }
+        })
+
+        router.push({ pathname: path, query: query });
+    
+    };
 
     return (
         <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
