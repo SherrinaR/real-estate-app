ddb0871e69866a70cb014e2617ebd263948eb48c
Sherrina <robinsonsherrina@gmail.com>
2023-05-23 13:45:30 -0400

    added property filters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -4,11 +4,34 @@ import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
+import { filterData, getFilterValues } from '../utils/filterData';
+
 const SearchFilters = () => {
+    const [filters, setFilters] = useState(filterData);
+
+    const searchProperties = (filterValues) => {
+
+    }
+
     return (
-        <div>
-            searchFilters
-        </div>
+        <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+      {filters?.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select 
+            onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })} 
+            placeholder={filter.placeholder} 
+            w='fit-content' 
+            p='2' 
+            >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+      </Flex>
     )
 }
 
